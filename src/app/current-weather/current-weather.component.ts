eef1c9067860de7c1524d07971712a362b2775af
Kal <info@kalacademy.org>
2020-05-09 14:56:36 -0700

    weather service

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ICurrentWeather } from '../icurrent-weather';
 
 @Component({
   selector: 'app-current-weather',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-weather.component.css']
 })
 export class CurrentWeatherComponent implements OnInit {
+  current: ICurrentWeather
+  constructor() {
+    this.current = {
+      city: 'Redmond',
+      country: 'US',
+      date: new Date(),
+      image: '',
+      temperature: 85,
+      description: 'sunny'
+    }
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
