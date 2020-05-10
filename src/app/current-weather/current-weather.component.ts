234751cf6e7557204554e37332611b02d75facc1
Kal <info@kalacademy.org>
2020-05-10 14:50:37 -0700

    completed current weather and test

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,26 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ICurrentWeather } from '../icurrent-weather';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.css']
+  styleUrls: ['./current-weather.component.css'],
 })
 export class CurrentWeatherComponent implements OnInit {
-  current: ICurrentWeather
-  constructor() {
-    this.current = {
-      city: 'Redmond',
-      country: 'US',
-      date: new Date(),
-      image: '',
-      temperature: 85,
-      description: 'sunny'
-    }
-
-  }
+  current: ICurrentWeather;
+  constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
+    this.weatherService
+      .getCurrentWeather('Redmond', 'US')
+      .subscribe((data) => (this.current = data));
   }
-
 }
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { ICurrentWeather } from './icurrent-weather';
+import {map} from 'rxjs/operators';
 
 interface ICurrentWeatherData {
   weather: [
@@ -29,7 +31,24 @@ export class WeatherService {
 
   getCurrentWeather(city: string, country: string){
     return this.httpClient.get<ICurrentWeatherData>(
-      `${environment.baseUrl}api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${environment.appId}`
-    )
+      `${environment.baseUrl}api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${environment.appId}`).pipe(
+        map(data => this.transformToICurrentWeather(data))
+      )
+
+  }
+
+  transformToICurrentWeather(data: ICurrentWeatherData) : ICurrentWeather{
+    return {
+      city: data.name,
+      country: data.sys.country,
+      date: new Date(data.dt * 1000),
+      image: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
+      temperature: this.convertKelvinToFahrenheit(data.main.temp),
+      description: data.weather[0].description
+    }
+  }
+
+  convertKelvinToFahrenheit(kelvin: number): number{
+    return kelvin * 9 / 5 - 459.67
   }
 }
