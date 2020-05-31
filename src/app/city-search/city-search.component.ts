d017c2fe015ba0443aa7bfb83c8de4ca221ecad1
Kal <info@kalacademy.org>
2020-05-31 11:56:54 -0700

    final commit

--- src/app/city-search/city-search.component.ts
+++ src/app/city-search/city-search.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl} from '@angular/forms';
-import { WeatherService } from '../weather.service';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+import {debounceTime, debounce} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-city-search',
@@ -8,17 +9,19 @@ import { WeatherService } from '../weather.service';
   styleUrls: ['./city-search.component.css']
 })
 export class CitySearchComponent implements OnInit {
-  search = new FormControl();
-  constructor(private weatherService: WeatherService) { }
+
+  @Output() searchEvent = new EventEmitter<string>()
+
+  search = new FormControl('', [Validators.minLength(3)]);
+  constructor() { }
 
   ngOnInit(): void {
     this.search.valueChanges
+      .pipe(debounceTime(1000))
       .subscribe((searchValue: string) => {
-        if (searchValue){
-          const userInput = searchValue.split(',')
-                              .map(s => s.trim())
-          this.weatherService.getCurrentWeather(userInput[0],
-            userInput.length > 1 ? userInput[1] : undefined).subscribe(data => console.log(data));
+        if (searchValue && !this.search.invalid){
+          this.searchEvent.emit(searchValue);
+
         }
       })
   }
